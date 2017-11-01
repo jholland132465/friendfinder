1d2ae1ec05b2f879e8cf8062d01c084a163c7f1b
Joe <joe@Joes-MacBook-Pro.local>
2017-11-01 15:47:27 -0700

    3rd

---
+++ app/data/friends.js
@@ -0,0 +1,133 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Agatha',
+		photo: '../public/assets/agatha.gif',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Clark',
+		photo: '../public/assets/clark.gif',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jane',
+		photo: '../public/assets/jane.gif',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Frederick',
+		photo: '../public/assets/frederick.gif',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Mrs. Havisham',
+		photo: '../public/assets/havisham.gif',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Dale',
+		photo: '../public/assets/dale.gif',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Oscar',
+		photo: '../public/assets/oscar.gif',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Hunny',
+		photo: '../public/assets/hunny.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
