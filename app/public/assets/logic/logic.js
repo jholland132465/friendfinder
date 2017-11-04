b625ac761b54861a82aa4dc504a157f77e3ec7e4
Joe <joe@joes-mbp.attlocal.net>
2017-11-04 08:40:39 -0700

    9th or something

--- app/public/assets/logic/logic.js
+++ app/public/assets/logic/logic.js
@@ -1,39 +1,24 @@
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('select').select();
-		$('.modal').modal({
-			dismissible: false, 
-			startingTop: '5%',
-			});
-		});
-
-		$('#submitButton').on('click', function(event) {
+$(document).ready(function() {
+	$('#submitButton').on('click', function(event) {
 			event.preventDefault();
 			var userInput = {
-				name: $('#userName').val().trim(),
-				photo: $('#userPhoto').val().trim(),
+				name: $('#userName'),
+				photo: $('#userPhoto'),
 				scores:[
-					$('#question1').val().trim(),
-					$('#question2').val().trim(),
-					$('#question3').val().trim(),
-					$('#question4').val().trim(),
-					$('#question5').val().trim(),
-					$('#question6').val().trim(),
-					$('#question7').val().trim(),
-					$('#question8').val().trim(),
-					$('#question9').val().trim(),
-					$('#question10').val().trim()
+					$('#question1'),
+					$('#question2'),
+					$('#question3'),
+					$('#question4'),
+					$('#question5')
 				]
 			};
 
 			$.post('/api/friends', userInput)
       		.done(function(data) {
-      		
       			$('#userMatch').html(data.matchName);
 		    	$('#userMatchImage').attr("src", data.matchImage);
-		    	$('<img src="../agatha.gif">').appendTo("#matchreveal");
-
-
       		});
-		});
-	</script>
+
+		$('<img src="../agatha.gif">').appendTo("#matchreveal");
+				});
+});
